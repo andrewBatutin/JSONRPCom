6621c5f05becc1401c8a26c311e96e4bd52d005f
Andriy Batutin <andriy.batutin@email.com>
2016-11-12 11:49:08 +0200

    Initial Commit

---
+++ JSONRPCom/JSONRPCom.h
@@ -0,0 +1,19 @@
+//
+//  JSONRPCom.h
+//  JSONRPCom
+//
+//  Created by andrew batutin on 11/12/16.
+//  Copyright © 2016 HomeOfRisingSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONRPCom.
+FOUNDATION_EXPORT double JSONRPComVersionNumber;
+
+//! Project version string for JSONRPCom.
+FOUNDATION_EXPORT const unsigned char JSONRPComVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONRPCom/PublicHeader.h>
+
+
